52de854f6bf2ec0c118ba44de4eccbb14380fc19
Shapoval <olekcandrsapoval41@gmail.com>
2022-11-26 19:55:19 +0200

    Ex_1.1

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-start = int(input("Ведіть перше число --->"))
-end = int(input("Ведіть друге число --->"))
-while start < end:
-    if int(start % 7)==0:
-        print(f"Number {start} is multiple 7")
-    start += 1
+try:
+    start = int(input("Ведіть перше число --->"))
+    end = int(input("Ведіть друге число --->"))
+    while start < end:
+        if int(start % 7)==0:
+            print(f"Number {start} is multiple 7")
+        start += 1
+except Exception as e:
+    print(f"Error --> {e}")
