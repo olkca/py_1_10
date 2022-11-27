7b1193c70400168bd7893ac861f745f787cd03b0
Shapoval <olekcandrsapoval41@gmail.com>
2022-11-27 12:31:27 +0200

    Ex_3.1

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-start = int(input("Ведіть перше число --->"))
-end = int(input("Ведіть друге число --->"))
-while start < end:
-    if int(start % 3)==0:
-        print("Fizz")
-    if int(start % 5)==0:
-        print("Buzz")
-    if int(start % 5)==0:
-        if int(start % 3)==0:
-            print("Fizz Buzz")
-    if int(start % 5)==1:
-        if int(start % 3)==1:
+try:
+    start = int(input("Ведіть перше число --->"))
+    end = int(input("Ведіть друге число --->"))
+    while start < end:
+        if (start % 5)==0:
+            if (start % 3) == 0:
+                print(f"Fizz Buzz{start}")
+                start += 1
+        if (start % 3)==0:
+            print(f"Fizz{start}")
+            start += 1
+        if (start % 5)==0:
+            print(f"Buzz{start}")
+            start += 1
+        if start % 5 and start % 3 == 1:
             print(start)
             start += 1
-    start += 1
+        if start % 5 and start % 3 == 2:
+            print(start)
+            start += 1
+except Exception as e:
+        print(f"Error --> {e}")
+
 
